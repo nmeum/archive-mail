172be6b79c0bf85db68891ee021afb71a92874f8
Sören Tempel <soeren+git@soeren-tempel.net>
2020-02-09 12:53:00 +0100

    tests: fix unmodified test case

--- tests/0000:unmodified.sh
+++ tests/0000:unmodified.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . "${0%/*}/common.sh"
 
-add_mail current/cur/1:2, expected/cur/1:2, expected/cur/1:2, <<-EOF
+add_mail current/cur/1:2, archive/cur/1:2, expected/cur/1:2, <<-EOF
 	From: Some One <example@example.com>
 	Subject: Unmodified Message v1
 	Date: Mon, 23 Dez 2313 12:23:42 +0200
@@ -10,7 +10,7 @@ add_mail current/cur/1:2, expected/cur/1:2, expected/cur/1:2, <<-EOF
 	This message is already in the archive.
 EOF
 
-add_mail current/cur/2:2, expected/cur/2:2, expected/cur/2:2, <<-EOF
+add_mail current/cur/2:2, archive/cur/2:2, expected/cur/2:2, <<-EOF
 	From: Some One <example@example.com>
 	Subject: Unmodified Message v2
 	Date: Mon, 23 Dez 2314 12:23:42 +0200
